ef7762324d6361c2a06a95a4092ba6e3f0b87918
Nick Chemsak <nchemsak@gmail.com>
2017-02-20 15:22:05 -0600

    updated views and urls with a href links

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+
+def about(request):
+    return HttpResponse("<h1>Rango says here is the about page.</h1> <br/> <a href='/rango/'>Index</a>")
